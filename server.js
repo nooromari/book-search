8e08dba8b6a4aaccc4999362f63f4ada0a050228
NOOR OMARI <nooromari847@gmail.com>
2021-04-04 22:34:53 +0300

    add books from search

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static('public/styles'));
+app.use(errorHandler);
 
 app.set('view engine', 'ejs');
 
@@ -20,6 +21,14 @@ app.post('/searches', createSearch);
 
 app.get('*', (request, response) => response.status(404).send('This route does not exist'));
 
+function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(500);
+  res.render('pages/error', { error: err });
+}
+
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
 
 function renderHomePage(request, response) {
@@ -27,7 +36,7 @@ function renderHomePage(request, response) {
 }
 
 function showForm(request, response) {
-  response.render('pages/searches/show.ejs');
+  response.render('pages/searches/new');
 }
 
 function createSearch(request, response) {
@@ -42,13 +51,32 @@ function createSearch(request, response) {
 
   superagent.get(url)
     .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
-    .then(results => response.render('pages/show', { searchResults: results }));
+    .then(results => response.render('pages/searches/show', { searchResults: results }))
+    .catch(error => response.status(500).send(`somthing wrong ${error}`));
+  // const search = request.body.search[0];
+  // let url = `https://www.googleapis.com/books/v1/volumes?q=${search}`;
+  // let url = `https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:`;
+
+  // console.log(url);
+  // console.log(request.body.search);
+  // superagent.get(url).then(data=> response.send(data.body.items));
+
+  // can we convert this to ternary?
+  // if (request.body.search[1] === 'title') { url += `+intitle:${request.body.search[0]}`; }
+  // if (request.body.search[1] === 'author') { url += `+inauthor:${request.body.search[0]}`; }
+
+  // superagent.get(url)
+  //   .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+  //   .then(results => response.render('pages/searches/show', { searchResults: results }));
   // how will we handle errors?
+  // response.render('pages/searches/show');
+
 }
 
 function Book(info) {
-  const placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
-
+  // this.image = info.imageLinks.smallThumbnail ||'https://i.imgur.com/J5LVHEL.jpg';
   this.title = info.title || 'No title available'; // shortcircuit
-
+  this.author = info.authors[0];
+  this.description = info.description;
+  this.image = (info.imageLinks) ? info.imageLinks.smallThumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
 }
